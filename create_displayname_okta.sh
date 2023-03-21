6cf0da36d65cbd46eda62386fd3decc6987835fc
aalonso <aalonso@babbel.com>
2023-03-21 12:23:25 +0100

    modified create_display_name to be more efficient

--- create_displayname_okta.sh
+++ create_displayname_okta.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Search for email address and bring back userid, firstName and lastName, then add a displayName using these values.
+# Usage: <script> <target csv>
 
 url="OKTA URL"
 
-
+# Function to modify the user
 updateuser() {
     curl --location -g --request POST "$url/api/v1/users/""$1" \
         --header 'Accept: application/json' \
@@ -17,19 +18,17 @@ updateuser() {
     }'
 }
 
-while read -r email; do
-    userid=$(curl --location "$url//api/v1/users?q=$email&limit=1" \
+# Function to search for a user based on email address
+getuserinfo() {
+    curl --location "$url//api/v1/users?q=$1&limit=1" \
         --header 'Accept: application/json' \
         --header 'Content-Type: application/json' \
-        --header "Authorization: SSWS $apikey" | jq -r '[.[0] | .id]' | tr -d '"','[]','\n' | awk '{$1=$1};1')
-    firstName=$(curl --location "$url//api/v1/users?q=$email&limit=1" \
-        --header 'Accept: application/json' \
-        --header 'Content-Type: application/json' \
-        --header "Authorization: SSWS $apikey" | jq -r '[.[0] | .profile.firstName]' | tr -d '"','[]','\n' | awk '{$1=$1};1')
-    lastName=$(curl --location "$url//api/v1/users?q=$email&limit=1" \
-        --header 'Accept: application/json' \
-        --header 'Content-Type: application/json' \
-        --header "Authorization: SSWS $apikey" | jq -r '[.[0] | .profile.lastName]' | tr -d '"','[]','\n' | awk '{$1=$1};1')
-    updateuser "$userid" "$firstName" "$lastName" &>/dev/null
-done <"$1"
+        --header "Authorization: SSWS $apikey" | jq -r '[.[0] | .id, .profile.firstName, .profile.lastName] | @csv' | tr -d '"'
+}
 
+# Main loop
+while read -r email; do
+    while IFS="," read -r id firstName lastName; do
+        updateuser "$id" "$firstName" "$lastName" &>/dev/null
+    done < <(getuserinfo "$email")
+done <"$1"
